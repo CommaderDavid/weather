6b7e579f57de2948c0c46d5afd56e8c7d563a147
David Boedigheimer <stealth259@gmail.com>
2020-04-13 14:30:25 -0700

    Add edit for async and await.

--- weather-interface.js
+++ weather-interface.js
@@ -3,15 +3,15 @@ $(document).ready(function() {
     const city = $('#location').val();
     $('#location').val("");
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[[API-KEY-GOES-HERE]]`)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(jsonifiedResponse) {
-        getElements(jsonifiedResponse);
-      });
+    asyncApiCall();
 
-   const getElements = function(response) {
+    async function asyncApiCall() {
+      let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[API-KEY-GOES-HERE]`;
+      let jsonifiedResponse = await response.json();
+      getElements(jsonifiedResponse);
+    }
+
+    const getElements = function(response) {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
     }
